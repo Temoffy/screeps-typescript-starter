dd07d70d8f6212ed927514b22fe10391059ecdeb
Telos <Temoffy@gmail.com>
2026-07-16 11:47:52 -0500

    spawner system basics done!

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,14 @@ screeps bot rewrite based on https://github.com/screepers/screeps-typescript-sta
 reason: had enough of type errors in runtime, implement task manager type systems
 */
 
+// main.ts
+
 import { WorldAtlas } from "WorldAtlas";
 import { JobBoard } from "JobBoard";
 import Hud from "utils/Hud";
-import {Tools} from "utils/Tools";
 import { creepHandler } from "foremen/CreepHandler";
-import { Prioritizer } from "Prioritizer";
+import { Scheduler } from "Scheduler";
+import { muster } from "muster/muster";
 
 declare global {
   interface Memory {
@@ -23,9 +25,8 @@ declare global {
   }
 
   interface Creep {
-    _say: (message: string, public?: boolean) => 0|-1|-4;
+    _say: (message: string, public?: boolean) => 0 | -1 | -4;
   }
-
 }
 // Syntax for adding properties to `global` (ex "global.log")
 declare const global: {
@@ -34,41 +35,61 @@ declare const global: {
     atlas: WorldAtlas;
     jobBoard: JobBoard;
     hud: Hud;
-  }
-}
+  };
+};
 
 // monkeypatching
 // eslint-disable-next-line no-underscore-dangle, @typescript-eslint/unbound-method
 const _say = Creep.prototype.say;
-Creep.prototype.say = function(message, sayPublic = true) {
-    return _say.call(this, message, sayPublic);
+Creep.prototype.say = function (message, sayPublic = true) {
+  return _say.call(this, message, sayPublic);
 };
 
 // declare my global variables, used 'g' instead of 'global' because it's shorter and I'm lazy.
-global.g = {atlas: new WorldAtlas(), jobBoard: new JobBoard(), hud: new Hud()};
+global.g = { atlas: new WorldAtlas(), jobBoard: new JobBoard(), hud: new Hud() };
 
-console.log("Hello World!")
+console.log("Hello World!");
 
-for( const room in Game.rooms){
-  g.atlas.SurveyRoom(room)
+for (const room in Game.rooms) {
+  g.atlas.SurveyRoom(room);
 }
-g.atlas.WriteMem()
+g.atlas.WriteMem();
 
-const prioritizer = new Prioritizer();
-for(const updateFunction of creepHandler.getUpdateFunctions()){
-  prioritizer.schedule(updateFunction.func, updateFunction.name, 1, 10);
+const scheduler = new Scheduler();
+for (const updateFunction of creepHandler.getUpdateFunctions()) {
+  scheduler.schedule(updateFunction.func, updateFunction.name, 1, 10);
 }
 
-module.exports.loop = function (){
+muster.addRequestSource(() => [
+  {
+    suffix: "test",
+    urgency: 2,
+    bodyOptions: [[MOVE, TOUGH]],
+    time: Game.time,
+    room: Game.spawns.Spawn1.pos.roomName,
+    maxBodyRepeat: 1
+  }
+]);
+
+module.exports.loop = function () {
   g.hud.makeElement("", Game.spawns.Spawn1.pos);
-  prioritizer.run();
-  for(const creepId in Game.creeps){
+  scheduler.run();
+  for (const creepId in Game.creeps) {
     const creep = Game.creeps[creepId];
-    if(creep.spawning) continue;
+    if (creep.spawning) continue;
     creepHandler.assignCreep(creep);
   }
+
+  for (const room in Game.rooms) {
+    g.atlas.SurveyRoom(room);
+  }
+  g.atlas.WriteMem();
+
   creepHandler.run();
-  prioritizer.writeMem();
+  scheduler.writeMem();
+
+  muster.runSpawning();
 
   g.hud.display();
-}
+  console.log("Loop finished");
+};
